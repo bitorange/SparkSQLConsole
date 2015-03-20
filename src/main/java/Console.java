75e2dbd042a37ff1d895b17538b7cf3c965ad428
bitorange <bitorange@foxmail.com>
2015-03-20 11:53:10 +0800

    Initial commit

---
+++ src/main/java/Console.java
@@ -0,0 +1,166 @@
+/**
+ * Created by orange on 2015/3/16.
+ */
+
+
+import dnl.utils.text.table.TextTable;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+
+public class Console {
+
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                // IDE中输入与输出
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Please input your name:");
+                String name = sc.nextLine();
+                System.out.print("Please input your password:");
+                String password = sc.nextLine();
+
+               //控制台中输入与输出
+//              java.io.Console cons = System.console();
+//              String name=null,password=null;
+//              if (cons != null)
+//              {              // 判断是否有控制台的使用权
+//                  name = new String(cons.readLine("Please input your name: "));      // 读取整行字符
+//                  password = new String(cons.readPassword("Please input your password: "));   // 读取密码,输入时不显示
+//              }
+                //连接服务器验证用户名与密码
+                NetworkInterface net = new NetworkInterface();
+                //对字符串进行utf-8编码
+                String geturl = "check?name=" + URLEncoder.encode(name, "utf-8") + "&password=" + password;
+
+                String state = net.execute(geturl);
+
+                ArrayList<String> commandList=new ArrayList<String>();
+                if (state.equals("ok"))//用户名，密码正确
+                {
+                    System.out.println("登录成功");
+
+                    //继续输入sql语句等
+                   // System.out.print("SQL> ");
+                    String lines = null;
+                    System.out.print("SQL> ");
+                    while (!(lines = sc.nextLine()).equals("quit"))//如果不输入quit则一直输入
+                    {
+                      //  if(lines.equals())
+                        commandList.add(lines);
+                        try {
+
+                            String json = net.execute(URLEncoder.encode(lines, "utf-8"));
+
+                          //  System.out.println(json);
+                            /*json数据解析并打印*/
+                            Console console = new Console();
+                            console.jsonparser(json);
+                        }catch (Exception e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
+
+                        System.out.print("SQL> ");
+
+
+                        int position=commandList.size()-1;   //上一个String字符位置
+
+                        //监听键盘事件
+
+                    }
+                    break;
+                } else if (state.equals("no"))//用户名，密码不正确
+                {
+                    System.out.println("用户名与密码不匹配，请再次输入");
+                    // System.out.println(state);
+                    //再次输入用户名密码
+                    continue;
+                } else {//出现问题（如传输过程等问题）
+                    System.out.println(state);
+                    //再次输入用户名密码
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    /**
+     * 将获得的json数据进行解析，并显示在界面上
+     *
+     * @param     json  json字符串
+     * @return    void
+     * @exception UnsupportedEncodingException
+     * If the named encoding is not supported
+     */
+    private void jsonparser(String json){
+        //获取整个json字符串对象
+        JSONObject jsonObj = JSONObject.fromObject(json);
+
+
+        String code = jsonObj.getString("code");
+        String msg = jsonObj.getString("msg");
+        String time = jsonObj.getString("time");
+        String size = jsonObj.getString("size");
+
+      // System.out.println(code+"  "+msg+"  "+time+"  "+size);
+
+
+        if(msg.equals("success")) {
+            //获取字符串对象中的result数组
+            JSONArray jsonArray = jsonObj.getJSONArray("result");
+            if (null != jsonArray && jsonArray.size() > 0) {
+                List<Map<String, Object>> mapListJson = (List) jsonArray;
+
+                //得到表头
+                ArrayList<String> headerList = new ArrayList<String>();
+                Map<String, Object> obj1 = mapListJson.get(0);
+                //需要优化
+                for (Map.Entry<String, Object> entry : obj1.entrySet()) {
+                    String strkey1 = entry.getKey();    // 表头
+                    headerList.add(strkey1);
+                }
+                //表头数组
+                String[] header = new String[headerList.size()];
+                header = headerList.toArray(header);
+
+
+                //得到表中数据内容存在二维数组中
+                String[][] data = new String[mapListJson.size()][headerList.size()];
+                int j;
+                for (int i = 0; i < mapListJson.size(); i++) {
+                    Map<String, Object> obj = mapListJson.get(i);
+                    j = 0;
+                    for (Map.Entry<String, Object> entry : obj.entrySet()) {
+                        Object strval1 = entry.getValue();
+                        data[i][j++] = strval1.toString();
+                    }
+                }
+                // 生成TextTable
+                TextTable tt = new TextTable(header, data);
+                // this adds the numbering on the left
+               // tt.setAddRowNumbering(true);
+                // sort by the first column
+                //tt.setSort(0);
+                tt.printTable();
+                System.out.println();
+                System.out.println("time:" + time + "   size:" + size);
+            }
+        }
+        else{
+            System.out.println(msg);
+        }
+        // System.out.println(json);
+    }
+}
---
+++ src/main/java/NetworkInterface.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by orange on 2015/3/16.
+ */
+public class NetworkInterface {
+    private static final String targetURL = "http://localhost:8080/SparkSQLServer/service/";
+//    HttpURLConnection httpConnection;
+
+    public String execute(String geturl) {
+        String lines = null;
+        try {
+            URL targetUrl = new URL(targetURL + geturl);
+
+            /* 返回不同的URLConnection子类的对象，这里URL是一个http，因此实际返回的是HttpURLConnection */
+            //     HttpURLConnection connection = (HttpURLConnection) targetUrl.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) targetUrl.openConnection();
+
+            // 进行连接，但是实际上get request要在下一句的httpConnection.getInputStream()函数中才会真正发到服务器
+            httpConnection.connect();
+
+            // 取得输入流，并使用Reader读取
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+
+            //获取的输入流肯定只有一行
+
+            lines = reader.readLine();
+
+//            while ((lines = reader.readLine()) != null) {
+//                System.out.println(lines);
+//            }
+            reader.close();
+
+            // 断开连接
+            httpConnection.disconnect();
+
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+            lines=e.getMessage();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            lines=e.getMessage();
+        }
+        return lines;
+    }
+
+}
