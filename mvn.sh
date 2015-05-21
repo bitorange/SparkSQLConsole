301201c5534e1a3f89f19057bce3e62c9070dfe2
ihainan <ihainan72@gmail.com>
2015-05-21 11:30:32 +0800

    Fix: 修改 maven 项目导外部 Jar 包会抛 NoClassDefFoundError 异常的 bug

--- mvn.sh
+++ mvn.sh
@@ -1,1 +1,2 @@
+mvn install:install-file -Dfile=lib/j-text-utils.jar -DgroupId=dnl -DartifactId=j-text-utils -Dversion=0.3.3 -Dpackaging=jar
 mvn clean install -Dmaven.compiler.target=1.7 -Dmaven.compiler.source=1.7 -DskipTests=true -B -X
